d21a9df5f31798996b638dba029cc0b4527f6d38
zhangtingyu11 <18013933973@163.com>
2023-05-28 14:59:03 +0800

    增加clocs计算overlaps和tensor_idx的代码

--- pcdet/models/fusion_heads/clocs_head.py
+++ pcdet/models/fusion_heads/clocs_head.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch.nn as nn
 from ...utils.box_utils import boxes_to_corners_3d, pairwise_iou
 from ...utils import box_utils, common_utils, calibration_kitti
+from mmdet.models.task_modules.assigners import BboxOverlaps2D
 
 import torch
 
@@ -25,6 +26,7 @@ class ClocsHead(nn.Module):
         self.fuse = nn.Sequential(*self.fuse)
         self.maxpool_dim = self.model_cfg.MAXPOOL_DIM
         self.maxpool = nn.MaxPool2d([self.model_cfg.MAXPOOL_DIM,1],1),
+        self.compute_ious = BboxOverlaps2D()
 
     def init_weights(self):
         pi = 0.01
@@ -33,6 +35,8 @@ class ClocsHead(nn.Module):
 
     def forward(self, data_dict):
         preds = data_dict['batch_box_preds']
+        pred_scores = data_dict['batch_cls_preds']
+        dis_to_lidar = torch.norm(preds[:, :, :2],p=2,dim=2,keepdim=True)/82.0
         batch_size, pred_num, box_size = preds.shape
         preds = preds.view(batch_size*pred_num, box_size)
         pred_corners = boxes_to_corners_3d(preds)
@@ -65,7 +69,7 @@ class ClocsHead(nn.Module):
         x_max = torch.clamp(x_max,min = torch.zeros(batch_size, 1).cuda(),max = img_width.unsqueeze(-1)).unsqueeze(-1)
         y_max = torch.clamp(y_max,min = torch.zeros(batch_size, 1).cuda(),max = img_height.unsqueeze(-1)).unsqueeze(-1)
         
-        anchor_project_on_image = torch.cat([x_min, y_min, x_max, y_max], dim=-1)
+        anchor_project_on_image = torch.cat([x_min, y_min, x_max, y_max, pred_scores], dim=-1)
         
         #TODO 先用gt 2D框试试看，CLOCs用的是预测框
         gt_corners = boxes_to_corners_3d(data_dict['gt_boxes'][:, :, :7].reshape(-1, 7).cpu().numpy())
@@ -73,8 +77,12 @@ class ClocsHead(nn.Module):
         gt_corners_homo = torch.cat([gt_corners, torch.ones((*gt_corners.shape[:2], 1)).cuda()], dim=-1)
         gt_corners_homo = gt_corners_homo.view(batch_size, -1, 4)
         
-        gt_corners_on_image = torch.einsum('bij,bjk->bik', gt_corners_homo, calib)
-        gt_corners_on_image = gt_corners_on_image[:, :, :2]/gt_corners_on_image[:, :, 2].unsqueeze(-1)
+        gt_corners_rect = torch.einsum('bij,bjk->bik', gt_corners_homo, lidar_to_rect_matrix)
+        
+        gt_corners_rect_homo = torch.cat([gt_corners_rect, torch.ones((*gt_corners_rect.shape[:2], 1)).cuda()], dim=-1)
+        
+        gt_corners_on_image = torch.einsum('bij,bjk->bik', gt_corners_rect_homo, calib_P2_T).view(-1, 3)
+        gt_corners_on_image = (gt_corners_on_image[:, :2].transpose(0,1)/gt_corners_on_image[:, 2].unsqueeze(0)).transpose(0,1)
         gt_corners_on_image = gt_corners_on_image.view(batch_size, -1, 8, 2)
         
         x_min, _ = torch.min(gt_corners_on_image[:, :, :, 0], dim=-1)
@@ -82,54 +90,22 @@ class ClocsHead(nn.Module):
         y_min, _ = torch.min(gt_corners_on_image[:, :, :, 1], dim=-1)
         y_max, _ = torch.max(gt_corners_on_image[:, :, :, 1], dim=-1)
         
-        batch_image_shape = data_dict['image_shape']
-        img_height = batch_image_shape[:, 0]
-        img_width = batch_image_shape[:, 1]
         x_min = torch.clamp(x_min,min = torch.zeros(batch_size, 1).cuda(),max = img_width.unsqueeze(-1)).unsqueeze(-1)
         y_min = torch.clamp(y_min,min = torch.zeros(batch_size, 1).cuda(),max = img_height.unsqueeze(-1)).unsqueeze(-1)
         x_max = torch.clamp(x_max,min = torch.zeros(batch_size, 1).cuda(),max = img_width.unsqueeze(-1)).unsqueeze(-1)
         y_max = torch.clamp(y_max,min = torch.zeros(batch_size, 1).cuda(),max = img_height.unsqueeze(-1)).unsqueeze(-1)
         
-        gt_project_on_image = torch.cat([x_min, y_min, x_max, y_max], dim=-1)
+        gt_project_on_image = torch.cat([x_min, y_min, x_max, y_max, torch.ones_like(x_min)], dim=-1)
         
-        ious = []
-        for bidx in range(batch_size):
-            ious.append(pairwise_iou(anchor_project_on_image[bidx], gt_project_on_image[bidx]))
-        
-        
-        
-        
-        spatial_features_2d = data_dict['spatial_features_2d']
-
-        cls_preds = self.conv_cls(spatial_features_2d)
-        box_preds = self.conv_box(spatial_features_2d)
-
-        cls_preds = cls_preds.permute(0, 2, 3, 1).contiguous()  # [N, H, W, C]
-        box_preds = box_preds.permute(0, 2, 3, 1).contiguous()  # [N, H, W, C]
-
-        self.forward_ret_dict['cls_preds'] = cls_preds
-        self.forward_ret_dict['box_preds'] = box_preds
-
-        if self.conv_dir_cls is not None:
-            dir_cls_preds = self.conv_dir_cls(spatial_features_2d)
-            dir_cls_preds = dir_cls_preds.permute(0, 2, 3, 1).contiguous()
-            self.forward_ret_dict['dir_cls_preds'] = dir_cls_preds
-        else:
-            dir_cls_preds = None
-
-        if self.training:
-            targets_dict = self.assign_targets(
-                gt_boxes=data_dict['gt_boxes']
-            )
-            self.forward_ret_dict.update(targets_dict)
-
-        if not self.training or self.predict_boxes_when_training:
-            batch_cls_preds, batch_box_preds = self.generate_predicted_boxes(
-                batch_size=data_dict['batch_size'],
-                cls_preds=cls_preds, box_preds=box_preds, dir_cls_preds=dir_cls_preds
-            )
-            data_dict['batch_cls_preds'] = batch_cls_preds
-            data_dict['batch_box_preds'] = batch_box_preds
-            data_dict['cls_preds_normalized'] = False
-
+        overlaps1 = torch.zeros((batch_size, 900000, 4), dtype=gt_project_on_image.dtype)-1
+        tensor_index1 = torch.zeros((batch_size, 900000, 2), dtype=gt_project_on_image.dtype)-1
+        for bidx, (anchor_projected, gt_projected) in enumerate(zip(anchor_project_on_image, gt_project_on_image)):
+            cur_iou = self.compute_ious(anchor_projected, gt_projected)
+            nonzero_indexes = cur_iou.nonzero()
+            overlaps1[bidx][:nonzero_indexes.shape[0], 0] = cur_iou[nonzero_indexes[:, 0], nonzero_indexes[:, 1]]
+            overlaps1[bidx][:nonzero_indexes.shape[0], 1] = torch.sigmoid(pred_scores[bidx][nonzero_indexes[:, 0]].squeeze(-1))
+            overlaps1[bidx][:nonzero_indexes.shape[0], 2] = gt_project_on_image[bidx][nonzero_indexes[:, 1], 4]
+            overlaps1[bidx][:nonzero_indexes.shape[0], 3] = dis_to_lidar[bidx][nonzero_indexes[:, 0], 0]
+            tensor_index1[bidx][:nonzero_indexes.shape[0], 0] = nonzero_indexes[:, 0]
+            tensor_index1[bidx][:nonzero_indexes.shape[0], 1] = nonzero_indexes[:, 1]
         return data_dict
