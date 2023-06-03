02fb304e776c8b24a23b0181f3bcf3978c1df93d
zhangtingyu11 <18013933973@163.com>
2023-06-03 18:40:19 +0800

    增加clocs的可用版本

--- pcdet/models/fusion_heads/clocs_head.py
+++ pcdet/models/fusion_heads/clocs_head.py
@@ -22,60 +22,60 @@ import copy
 #     xyz = lidar_to_camera(xyz_lidar, r_rect, velo2cam)
 #     return torch.cat([xyz, l, h, w, r], dim=-1)
 
-# @numba.jit(nopython=True,parallel=True)
-# def build_stage2_training(boxes, query_boxes, criterion, scores_3d, scores_2d, dis_to_lidar_3d,overlaps,tensor_index):
-#     N = boxes.shape[0] #70400
-#     K = query_boxes.shape[0] #30
-#     ind=0
-#     ind_max = ind
-#     for k in range(K):
-#         qbox_area = ((query_boxes[k, 2] - query_boxes[k, 0]) *
-#                      (query_boxes[k, 3] - query_boxes[k, 1]))
-#         for n in range(N):
-#             iw = (min(boxes[n, 2], query_boxes[k, 2]) -
-#                   max(boxes[n, 0], query_boxes[k, 0]))
-#             if iw > 0:
-#                 ih = (min(boxes[n, 3], query_boxes[k, 3]) -
-#                       max(boxes[n, 1], query_boxes[k, 1]))
-#                 if ih > 0:
-#                     if criterion == -1:
-#                         ua = (
-#                             (boxes[n, 2] - boxes[n, 0]) *
-#                             (boxes[n, 3] - boxes[n, 1]) + qbox_area - iw * ih)
-#                     elif criterion == 0:
-#                         ua = ((boxes[n, 2] - boxes[n, 0]) *
-#                               (boxes[n, 3] - boxes[n, 1]))
-#                     elif criterion == 1:
-#                         ua = qbox_area
-#                     else:
-#                         ua = 1.0
-#                     overlaps[ind,0] = iw * ih / ua
-#                     overlaps[ind,1] = scores_3d[n,0]
-#                     overlaps[ind,2] = scores_2d[k,0]
-#                     overlaps[ind,3] = dis_to_lidar_3d[n,0]
-#                     tensor_index[ind,0] = k
-#                     tensor_index[ind,1] = n
-#                     ind = ind+1
+@numba.jit(nopython=True,parallel=True)
+def compute_clocs_ious(boxes, query_boxes, criterion, scores_3d, scores_2d, dis_to_lidar_3d,overlaps,tensor_index):
+    N = boxes.shape[0] #70400
+    K = query_boxes.shape[0] #30
+    ind=0
+    ind_max = ind
+    for k in range(K):
+        qbox_area = ((query_boxes[k, 2] - query_boxes[k, 0]) *
+                     (query_boxes[k, 3] - query_boxes[k, 1]))
+        for n in range(N):
+            iw = (min(boxes[n, 2], query_boxes[k, 2]) -
+                  max(boxes[n, 0], query_boxes[k, 0]))
+            if iw > 0:
+                ih = (min(boxes[n, 3], query_boxes[k, 3]) -
+                      max(boxes[n, 1], query_boxes[k, 1]))
+                if ih > 0:
+                    if criterion == -1:
+                        ua = (
+                            (boxes[n, 2] - boxes[n, 0]) *
+                            (boxes[n, 3] - boxes[n, 1]) + qbox_area - iw * ih)
+                    elif criterion == 0:
+                        ua = ((boxes[n, 2] - boxes[n, 0]) *
+                              (boxes[n, 3] - boxes[n, 1]))
+                    elif criterion == 1:
+                        ua = qbox_area
+                    else:
+                        ua = 1.0
+                    overlaps[ind,0] = iw * ih / ua
+                    overlaps[ind,1] = scores_3d[n,0]
+                    overlaps[ind,2] = scores_2d[k,0]
+                    overlaps[ind,3] = dis_to_lidar_3d[n,0]
+                    tensor_index[ind,0] = k
+                    tensor_index[ind,1] = n
+                    ind = ind+1
 
-#                 elif k==K-1:
-#                     overlaps[ind,0] = -10
-#                     overlaps[ind,1] = scores_3d[n,0]
-#                     overlaps[ind,2] = -10
-#                     overlaps[ind,3] = dis_to_lidar_3d[n,0]
-#                     tensor_index[ind,0] = k
-#                     tensor_index[ind,1] = n
-#                     ind = ind+1
-#             elif k==K-1:
-#                 overlaps[ind,0] = -10
-#                 overlaps[ind,1] = scores_3d[n,0]
-#                 overlaps[ind,2] = -10
-#                 overlaps[ind,3] = dis_to_lidar_3d[n,0]
-#                 tensor_index[ind,0] = k
-#                 tensor_index[ind,1] = n
-#                 ind = ind+1
-#     if ind > ind_max:
-#         ind_max = ind
-#     return overlaps, tensor_index, ind
+                elif k==K-1:
+                    overlaps[ind,0] = -10
+                    overlaps[ind,1] = scores_3d[n,0]
+                    overlaps[ind,2] = -10
+                    overlaps[ind,3] = dis_to_lidar_3d[n,0]
+                    tensor_index[ind,0] = k
+                    tensor_index[ind,1] = n
+                    ind = ind+1
+            elif k==K-1:
+                overlaps[ind,0] = -10
+                overlaps[ind,1] = scores_3d[n,0]
+                overlaps[ind,2] = -10
+                overlaps[ind,3] = dis_to_lidar_3d[n,0]
+                tensor_index[ind,0] = k
+                tensor_index[ind,1] = n
+                ind = ind+1
+    if ind > ind_max:
+        ind_max = ind
+    return overlaps, tensor_index, ind
 class ClocsHead(AnchorHeadTemplate):
     def __init__(self, model_cfg, input_channels, num_class, class_names, grid_size, point_cloud_range,
                  predict_boxes_when_training=True, **kwargs):
@@ -97,20 +97,13 @@ class ClocsHead(AnchorHeadTemplate):
         self.fuse = nn.Sequential(*self.fuse)
         self.maxpool_dim = self.model_cfg.MAXPOOL_DIM
         self.maxpool = nn.MaxPool2d([self.model_cfg.MAXPOOL_DIM,1],1)
-        # self.init_weights()
-
-    def init_weights(self):
-        pi = 0.01
-        # nn.init.constant_(self.fuse.bias, -np.log((1 - pi) / pi))
-        nn.init.normal_(self.fuse.weight, mean=0, std=0.001)
-        # pass
 
     def forward(self, data_dict):
         preds = data_dict['batch_box_preds']
         #* 转成0~1之间
-        pred_scores_3d = torch.sigmoid(data_dict['batch_cls_preds'])
+        pred_3d_scores = torch.sigmoid(data_dict['batch_cls_preds']).detach().cpu().numpy()
         #! 计算包围框中心点到lidar的距离, clocs是计算包围框底部中心点到lidar的距离
-        dis_to_lidar = torch.norm(preds[:, :, :2],p=2,dim=2,keepdim=True)/82.0
+        dis_to_lidar = torch.norm(preds[:, :, :2],p=2,dim=2,keepdim=True).detach().cpu().numpy()/82.0
         
         calib_V2C_T = data_dict['calib_matrix_V2C_T']
         calib_R0_T = data_dict['calib_matrix_R0_T']
@@ -119,77 +112,83 @@ class ClocsHead(AnchorHeadTemplate):
         img_height = image_shape[:, 0]
         img_width = image_shape[:, 1]
         
-        box_preds_on_image = lidar_boxes_to_image_kitti_torch_cuda(preds, calib_P2_T, calib_R0_T,
-                                                                   calib_V2C_T, img_height, img_width)
+        #* 计算lidar坐标系下的包围框投影到图像上的坐标系
+        box_preds_on_image = lidar_boxes_to_image_kitti_torch_cuda(preds, 
+                                                                   calib_P2_T, 
+                                                                   calib_R0_T,
+                                                                   calib_V2C_T, 
+                                                                   img_height, 
+                                                                   img_width).detach().cpu().numpy()
         
-        boxes2d_by_detector = data_dict['results_2d']
-        
+        boxes2d_by_detector = data_dict['results_2d'].detach().cpu().numpy()
         cls_pred_list = []
         valid_flag = []
-        for (anchor_projected, boxes2d_projected, pred_score_3d, dis_to_lidar_single) in zip(box_preds_on_image, 
-                                                                        boxes2d_by_detector, 
-                                                                        pred_scores_3d,
-                                                                        dis_to_lidar):
-            boxes2d_projected_scores = boxes2d_projected[:, -1]
-            boxes2d_projected = boxes2d_projected[:, :4]
-            boxes_3d_num = anchor_projected.shape[0]
-            boxes_2d_num = boxes2d_projected.shape[0]
-            overlap = torch.zeros((boxes_3d_num, boxes_2d_num, 4), 
-                                  dtype = torch.float32,
-                                  device = anchor_projected.device)-1
-            tensor_index = torch.zeros((boxes_3d_num, boxes_2d_num, 2), 
-                                    dtype = torch.int,
-                                    device = anchor_projected.device)-1
-            overlap, tensor_index = compute_clocs_iou(anchor_projected,
-                                          boxes2d_projected.contiguous(),
-                                          pred_score_3d.squeeze(-1),
-                                          boxes2d_projected_scores.contiguous(),
-                                          dis_to_lidar_single.squeeze(-1),
-                                          overlap,
-                                          tensor_index)
-            tensor_index = torch.sum(tensor_index, dim=-1) != -2
-            nonzero_indexes = tensor_index.nonzero()
-            torch.sort(nonzero_indexes)
-            if(nonzero_indexes.shape[0] == 0):
-                non_empty_iou_test_tensor = torch.zeros(1,4,1,2)
-                non_empty_iou_test_tensor[:,:,:,:] = -1
-                non_empty_tensor_index_tensor = torch.zeros(2,2)
-                non_empty_tensor_index_tensor[:,:] = -1
-                output = torch.zeros(1,200,70400,dtype = non_empty_iou_test_tensor.dtype,device = non_empty_iou_test_tensor.device)-9999999
+        for box_2d_preds, box_2d_detector, scores_3d, dist_to_lidar_single in zip(box_preds_on_image, 
+                                                                                    boxes2d_by_detector,
+                                                                                    pred_3d_scores,
+                                                                                    dis_to_lidar):
+            cur_pred_2d = box_2d_detector
+            k = cur_pred_2d.__len__() - 1
+            while k >= 0 and cur_pred_2d[k].sum() == 0:
+                k -= 1
+            box_2d_detector = cur_pred_2d[:k + 1]
+            scores_2d = box_2d_detector[:, 4:5]
+            box_2d_detector = box_2d_detector[:, :4]
+            overlap = np.zeros((900000,4),dtype=box_2d_preds.dtype)-1
+            tensor_index = np.zeros((900000,2),dtype=box_2d_preds.dtype)-1
+            ious, tensor_idxs, max_num = compute_clocs_ious(box_2d_preds,
+                                                                box_2d_detector,
+                                                                -1,
+                                                                scores_3d,
+                                                                scores_2d,
+                                                                dist_to_lidar_single,
+                                                                overlap,
+                                                                tensor_index)
+
+            ious_tensor = torch.FloatTensor(ious).permute(1,0).reshape(1,4,1,900000)
+            tensor_idxs_tensor = torch.LongTensor(tensor_idxs).reshape(-1,2)
+            if max_num == 0:
+                non_empty_ious_tensor = torch.zeros(1,4,1,2)-1
+                non_empty_tensor_idxs_tensor = torch.zeros(2,2)-1
+            else:
+                non_empty_ious_tensor = ious_tensor[:,:,:,:max_num]
+                non_empty_tensor_idxs_tensor = tensor_idxs_tensor[:max_num,:]
+            non_empty_tensor_idxs_tensor = non_empty_tensor_idxs_tensor.cuda()
+            non_empty_ious_tensor = non_empty_ious_tensor.cuda()
+            if non_empty_tensor_idxs_tensor[0,0] == -1:
+                output = torch.zeros(1,200,70400,dtype = non_empty_ious_tensor.dtype,device = non_empty_ious_tensor.device)-9999999
                 valid_flag.append(False)
             else:
-                non_empty_iou_test_tensor = overlap[nonzero_indexes[:, 0], nonzero_indexes[:, 1]]
-                non_empty_iou_test_tensor = non_empty_iou_test_tensor.transpose(0, 1)[None, :, None, :]
-                non_empty_tensor_index_tensor = tensor_index[nonzero_indexes[:, 0], nonzero_indexes[:, 1]]
-                output = torch.zeros(1,200, 70400, dtype=non_empty_iou_test_tensor.dtype, device = non_empty_iou_test_tensor.device)-9999999
-                res = self.fuse(non_empty_iou_test_tensor)
-                output[:,nonzero_indexes[:,1],nonzero_indexes[:,0]] = res[0,:,0,:]
+                res = self.fuse(non_empty_ious_tensor)
+                output = torch.zeros(1,200,70400,dtype = non_empty_ious_tensor.dtype,device = non_empty_ious_tensor.device)-9999999
+                output[:,non_empty_tensor_idxs_tensor[:,0],non_empty_tensor_idxs_tensor[:,1]] = res[0,:,0,:]
                 valid_flag.append(True)
+                
             output = self.maxpool(output)
-            cls_preds = output.reshape(1, 70400, 1)
-            cls_pred_list.append(cls_preds)
+            output = output.squeeze().reshape(1,-1,1)
+            cls_pred_list.append(output)
+        cls_preds = torch.cat(cls_pred_list, dim=0)
         
-        batch_cls_preds = torch.cat(cls_pred_list, dim=0)
-        self.forward_ret_dict['cls_preds'] =batch_cls_preds
-        self.forward_ret_dict['valid_flag'] =valid_flag
+        self.forward_ret_dict['cls_preds'] = cls_preds
+        self.forward_ret_dict['valid_flag'] = valid_flag
+        gt_boxes = data_dict['gt_boxes'].detach().cpu().numpy()
+        gt_boxes_classes = np.expand_dims(gt_boxes[:, :, -1], axis=-1)
+        gt_boxes_in_camera = np.expand_dims(boxes3d_lidar_to_kitti_camera(gt_boxes[0], data_dict['calib'][0]), axis=0)
+        gt_boxes_in_camera = np.concatenate([gt_boxes_in_camera, gt_boxes_classes], axis=-1)
+        preds_in_lidar = copy.deepcopy(preds)
+        #* 在相机坐标系下的预测框
+        preds_in_camera = np.expand_dims(boxes3d_lidar_to_kitti_camera(preds[0].detach().cpu().numpy(), data_dict['calib'][0]), axis=0)
 
-        gt_boxes = data_dict['gt_boxes']
-        gt_boxes_class = gt_boxes[:, :, 7:8]
-        gt_boxes = np.expand_dims(boxes3d_lidar_to_kitti_camera(gt_boxes[0].detach().cpu().numpy(), data_dict['calib'][0]), axis=0)
-        pred_boxes = np.expand_dims(boxes3d_lidar_to_kitti_camera(preds[0].detach().cpu().numpy(), data_dict['calib'][0]), axis=0)
-        gt_boxes = torch.from_numpy(gt_boxes).cuda()
-        gt_boxes = torch.cat([gt_boxes, gt_boxes_class], dim=-1)
-        pred_boxes = torch.from_numpy(pred_boxes).cuda()
         if self.training:
             targets_dict = self.assign_targets(
-                preds=pred_boxes,
-                gt_boxes=gt_boxes
+                preds=preds_in_camera,
+                gt_boxes=gt_boxes_in_camera
             )
             self.forward_ret_dict.update(targets_dict)
             
         if not self.training or self.predict_boxes_when_training:
-            data_dict['batch_cls_preds'] = batch_cls_preds
-            data_dict['batch_box_preds'] = preds
+            data_dict['batch_cls_preds'] = cls_preds
+            data_dict['batch_box_preds'] = preds_in_lidar
             data_dict['cls_preds_normalized'] = False
 
         return data_dict
@@ -248,21 +247,6 @@ class ClocsHead(AnchorHeadTemplate):
 
         """
         targets_dict = self.target_assigner.assign_targets(
-            [preds.view((1, 200, 176, 1, 2, 7))], gt_boxes
+            [preds.reshape((1, 200, 176, 1, 2, 7))], gt_boxes
         )
         return targets_dict
-    
-    def generate_predicted_boxes(self, batch_size, cls_preds, box_preds, dir_cls_preds=None):
-        """
-        Args:
-            batch_size:
-            cls_preds: (N, H, W, C1)
-            box_preds: (N, H, W, C2)
-            dir_cls_preds: (N, H, W, C3)
-
-        Returns:
-            batch_cls_preds: (B, num_boxes, num_classes)
-            batch_box_preds: (B, num_boxes, 7+C)
-
-        """
-        return cls_preds, box_preds
