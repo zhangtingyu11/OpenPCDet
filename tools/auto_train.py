3a982028ef8e5ac038a9f2ce08a11caa4c99de17
zhangtingyu11 <18013933973@163.com>
2023-06-05 13:25:17 +0800

    修改最后10轮进行验证，增加自动训练的代码

---
+++ tools/auto_train.py
@@ -0,0 +1,27 @@
+import os
+from decimal import Decimal
+# os.system('python train.py --cfg_file cfgs/kitti_models/pv_rcnn.yaml --extra_tag origin')
+# os.system('python train.py --cfg_file cfgs/kitti_models/pointrcnn.yaml --extra_tag origin')
+# os.system('python train.py --cfg_file cfgs/kitti_models/PartA2_free.yaml --extra_tag origin')
+# os.system('python train.py --cfg_file cfgs/kitti_models/PartA2.yaml --extra_tag origin')
+
+yaml_file = "/home/public/zty/Project/DeepLearningProject/OpenPCDet/tools/cfgs/kitti_models/second_car_clocs_contra.yaml"
+car_line = 120
+
+target = Decimal('1')
+start = Decimal('0.001')
+increase = Decimal('10')
+while start <= target:
+    with open(yaml_file, 'r') as f:
+        lines = f.readlines()
+        pre, suff = lines[car_line].split(":")
+        suff = " " + str(start) + ",\n"
+        lines[car_line] = pre + ":" + suff
+    with open(yaml_file, 'w') as f:
+        for line in lines:
+            f.write(line)
+    os.system('python train.py --cfg_file cfgs/kitti_models/second_car_clocs_contra.yaml --fix_random_seed --extra_tag second_car_clocs_contra_weight' + str(start)+
+              ' --pretrained_model ../output/kitti_models/second_car/origin/ckpt/checkpoint_epoch_80.pth')
+    start *= increase
+os.system('python train.py --cfg_file cfgs/kitti_models/second_car_clocs.yaml --fix_random_seed --extra_tag epoch10' + \
+        ' --pretrained_model ../output/kitti_models/second_car/origin/ckpt/checkpoint_epoch_80.pth')
--- tools/train.py
+++ tools/train.py
@@ -41,7 +41,7 @@ def parse_config():
 
     parser.add_argument('--max_waiting_mins', type=int, default=0, help='max waiting minutes')
     parser.add_argument('--start_epoch', type=int, default=0, help='')
-    parser.add_argument('--num_epochs_to_eval', type=int, default=0, help='number of checkpoints to be evaluated')
+    parser.add_argument('--num_epochs_to_eval', type=int, default=10, help='number of checkpoints to be evaluated')
     parser.add_argument('--save_to_file', action='store_true', default=False, help='')
     
     parser.add_argument('--use_tqdm_to_record', action='store_true', default=False, help='if True, the intermediate losses will not be logged to file, only tqdm will be used')
