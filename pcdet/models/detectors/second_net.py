0c52dc5667c5a211ee792323548c15ec6534a0de
zhangtingyu11 <18013933973@163.com>
2023-06-06 09:59:25 +0800

    增加second和pointpillar的输出写入pickle文件

--- pcdet/models/detectors/second_net.py
+++ pcdet/models/detectors/second_net.py
@@ -1,5 +1,6 @@
 from .detector3d_template import Detector3DTemplate
-
+import torch
+import pickle
 
 class SECONDNet(Detector3DTemplate):
     def __init__(self, model_cfg, num_class, dataset):
@@ -18,6 +19,15 @@ class SECONDNet(Detector3DTemplate):
             }
             return ret_dict, tb_dict, disp_dict
         else:
+            if(self.model_cfg.get('SAVE_PKL', False)):
+                for frame_id, pred, score in zip(batch_dict['frame_id'], batch_dict['batch_box_preds'], batch_dict['batch_cls_preds']):
+                    if(self.model_cfg.POST_PROCESSING.get('OUTPUT_RAW_SCORE', False)):
+                        result_3d = torch.cat([pred, score], dim=-1)
+                    else:
+                        result_3d = torch.cat([pred, torch.sigmoid(score)], dim=-1)
+                    pkl_file = '../data/kitti/lidar_detector_data/'+ self.model_cfg.CONFIG_NAME + '/' + str(frame_id) + '.pkl'
+                    with open(pkl_file, 'wb') as f:
+                        pickle.dump(result_3d.detach().cpu().numpy(), f)
             pred_dicts, recall_dicts = self.post_processing(batch_dict)
             return pred_dicts, recall_dicts
 
