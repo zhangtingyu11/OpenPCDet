bff93002333c6fbc4bbcf60ae5a4291bc32079e5
Grapymage <18013933973@163.com>
2023-09-22 11:36:00 +0800

    second clocs使用focal loss

--- pcdet/models/fusion_heads/clocs_second_head.py
+++ pcdet/models/fusion_heads/clocs_second_head.py
@@ -6,20 +6,155 @@ from ..dense_heads.anchor_head_template import AnchorHeadTemplate
 import torch
 import numba
 import copy
-# class ResidualBlockForClocs(nn.Module):
-#     def __init__(self, in_channels, out_channels):
-#         super().__init__()
-#         self.conv1 = nn.Conv2d(in_channels, out_channels, 1)
-#         self.bn1 = nn.SyncBatchNorm(out_channels)
-#         self.relu1 = nn.ReLU()
-#         self.conv2 = nn.Conv2d(out_channels, out_channels, 1)
-#         self.bn2 = nn.SyncBatchNorm(out_channels)
-#         self.relu2 = nn.ReLU()
-#     def forward(self, x):
-#         output = self.relu1(self.bn1(self.conv1(x)))
-#         output = self.bn2(self.conv2(output))+x
-#         output = self.relu2(output)
-#         return output
+from abc import ABCMeta, abstractmethod
+
+def _sigmoid_cross_entropy_with_logits(logits, labels):
+  # to be compatible with tensorflow, we don't use ignore_idx
+  loss = torch.clamp(logits, min=0) - logits * labels.type_as(logits) # this is the original
+  #loss = torch.clamp(logits, min=0) - torch.exp(logits) * labels.type_as(logits)
+  loss += torch.log1p(torch.exp(-torch.abs(logits)))
+  loss_mask = (loss < 10000)
+  loss_mask = loss_mask.type(torch.FloatTensor).cuda()
+  loss = loss*loss_mask
+  # transpose_param = [0] + [param[-1]] + param[1:-1]
+  # logits = logits.permute(*transpose_param)
+  # loss_ftor = nn.NLLLoss(reduce=False)
+  # loss = loss_ftor(F.logsigmoid(logits), labels)
+  return loss
+def indices_to_dense_vector(indices,
+                            size,
+                            indices_value=1.,
+                            default_value=0,
+                            dtype=np.float32):
+  """Creates dense vector with indices set to specific value and rest to zeros.
+
+  This function exists because it is unclear if it is safe to use
+    tf.sparse_to_dense(indices, [size], 1, validate_indices=False)
+  with indices which are not ordered.
+  This function accepts a dynamic size (e.g. tf.shape(tensor)[0])
+
+  Args:
+    indices: 1d Tensor with integer indices which are to be set to
+        indices_values.
+    size: scalar with size (integer) of output Tensor.
+    indices_value: values of elements specified by indices in the output vector
+    default_value: values of other elements in the output vector.
+    dtype: data type.
+
+  Returns:
+    dense 1D Tensor of shape [size] with indices set to indices_values and the
+        rest set to default_value.
+  """
+  dense = torch.zeros(size).fill_(default_value)
+  dense[indices] = indices_value
+
+  return dense
+
+class Loss(object):
+  """Abstract base class for loss functions."""
+  __metaclass__ = ABCMeta
+
+  def __call__(self,
+               prediction_tensor,
+               target_tensor,
+               ignore_nan_targets=False,
+               scope=None,
+               **params):
+    """Call the loss function.
+
+    Args:
+      prediction_tensor: an N-d tensor of shape [batch, anchors, ...]
+        representing predicted quantities.
+      target_tensor: an N-d tensor of shape [batch, anchors, ...] representing
+        regression or classification targets.
+      ignore_nan_targets: whether to ignore nan targets in the loss computation.
+        E.g. can be used if the target tensor is missing groundtruth data that
+        shouldn't be factored into the loss.
+      scope: Op scope name. Defaults to 'Loss' if None.
+      **params: Additional keyword arguments for specific implementations of
+              the Loss.
+
+    Returns:
+      loss: a tensor representing the value of the loss function.
+    """
+    if ignore_nan_targets:
+      target_tensor = torch.where(torch.isnan(target_tensor),
+                                prediction_tensor,
+                                target_tensor)
+    return self._compute_loss(prediction_tensor, target_tensor, **params)
+
+  @abstractmethod
+  def _compute_loss(self, prediction_tensor, target_tensor, **params):
+    """Method to be overridden by implementations.
+
+    Args:
+      prediction_tensor: a tensor representing predicted quantities
+      target_tensor: a tensor representing regression or classification targets
+      **params: Additional keyword arguments for specific implementations of
+              the Loss.
+
+    Returns:
+      loss: an N-d tensor of shape [batch, anchors, ...] containing the loss per
+        anchor
+    """
+    pass
+class SigmoidFocalClassificationLoss(Loss):
+  """Sigmoid focal cross entropy loss.
+
+  Focal loss down-weights well classified examples and focusses on the hard
+  examples. See https://arxiv.org/pdf/1708.02002.pdf for the loss definition.
+  """
+
+  def __init__(self, gamma=2.0, alpha=0.25):
+    """Constructor.
+
+    Args:
+      gamma: exponent of the modulating factor (1 - p_t) ^ gamma.
+      alpha: optional alpha weighting factor to balance positives vs negatives.
+      all_zero_negative: bool. if True, will treat all zero as background.
+        else, will treat first label as background. only affect alpha.
+    """
+    self._alpha = alpha
+    self._gamma = gamma
+
+  def _compute_loss(self,
+                    prediction_tensor,
+                    target_tensor,
+                    weights,
+                    class_indices=None):
+    """Compute loss function.
+
+    Args:
+      prediction_tensor: A float tensor of shape [batch_size, num_anchors,
+        num_classes] representing the predicted logits for each class
+      target_tensor: A float tensor of shape [batch_size, num_anchors,
+        num_classes] representing one-hot encoded classification targets
+      weights: a float tensor of shape [batch_size, num_anchors]
+      class_indices: (Optional) A 1-D integer tensor of class indices.
+        If provided, computes loss only for the specified class indices.
+
+    Returns:
+      loss: a float tensor of shape [batch_size, num_anchors, num_classes]
+        representing the value of the loss function.
+    """
+    weights = weights.unsqueeze(2)
+    if class_indices is not None:
+      weights *= indices_to_dense_vector(class_indices,
+            prediction_tensor.shape[2]).view(1, 1, -1).type_as(prediction_tensor)
+    per_entry_cross_ent = (_sigmoid_cross_entropy_with_logits(
+        labels=target_tensor, logits=prediction_tensor))
+    prediction_probabilities = torch.sigmoid(prediction_tensor)
+    p_t = ((target_tensor * prediction_probabilities) +
+           ((1 - target_tensor) * (1 - prediction_probabilities)))
+    modulating_factor = 1.0
+    if self._gamma:
+      modulating_factor = torch.pow(1.0 - p_t, self._gamma)
+    alpha_weight_factor = 1.0
+    if self._alpha is not None:
+      alpha_weight_factor = (target_tensor * self._alpha +
+                              (1 - target_tensor) * (1 - self._alpha))
+    focal_cross_entropy_loss = (modulating_factor * alpha_weight_factor*per_entry_cross_ent)
+    return focal_cross_entropy_loss * weights
         
 class ClocsSECONDHead(AnchorHeadTemplate):
     def __init__(self, model_cfg, input_channels, num_class, class_names, grid_size, point_cloud_range,
@@ -36,15 +171,12 @@ class ClocsSECONDHead(AnchorHeadTemplate):
         
         self.fuse = []
         for i in range(1, len(num_filters)-1):
-            # if(num_filters[i-1] == num_filters[i]):
-            #     self.fuse.append(ResidualBlockForClocs(num_filters[i-1], num_filters[i]))
-            # else:
-                self.fuse.append(nn.Conv2d(num_filters[i-1], num_filters[i], 1))
-                self.fuse.append(nn.ReLU())
+            self.fuse.append(nn.Conv2d(num_filters[i-1], num_filters[i], 1))
+            self.fuse.append(nn.ReLU())
+            
+        #* 因为希望最后的结果不是全部大于0的, 所以没有加ReLU
         self.fuse.append(nn.Conv2d(num_filters[-2], num_filters[-1], 1))
         self.fuse = nn.Sequential(*self.fuse)
-        self.maxpool_dim = self.model_cfg.MAXPOOL_DIM
-        self.maxpool = nn.MaxPool2d([self.model_cfg.MAXPOOL_DIM,1],1)
 
     def forward(self, data_dict):
         #* 读取3D目标检测的结果
@@ -69,16 +201,9 @@ class ClocsSECONDHead(AnchorHeadTemplate):
                                                                    img_height, 
                                                                    img_width)
         
+        #* 2D目标检测的结果
         boxes2d_by_detector = data_dict['results_2d']
-        # boxes_2d_num = boxes2d_by_detector.shape[1]
-        # if(boxes_2d_num >= 200):
-        #     boxes2d_by_detector = boxes2d_by_detector[:, :200, :]
-        # else:
-        #     padding = torch.zeros([boxes2d_by_detector.shape[0], 200-boxes_2d_num, boxes2d_by_detector.shape[-1]], 
-        #                            device = boxes2d_by_detector.device, 
-        #                            dtype = boxes2d_by_detector.dtype)
-        #     boxes2d_by_detector = torch.cat([boxes2d_by_detector, padding], dim=1)
-        # data_dict["results_2d"] = boxes2d_by_detector
+        #* 2D目标检测的个数
         boxes_2d_num = boxes2d_by_detector.shape[1]
         cls_pred_list = []
         for box_2d_preds, box_2d_detector, scores_3d, dist_to_lidar_single in zip(box_preds_on_image, 
@@ -89,20 +214,19 @@ class ClocsSECONDHead(AnchorHeadTemplate):
             box_2d_detector = box_2d_detector[:, :4]
             boxes_3d_num = box_2d_preds.shape[0]
             overlap = torch.zeros((boxes_3d_num, boxes_2d_num, 4), dtype = box_2d_detector.dtype, device = box_2d_detector.device)-1
-            # overlap = torch.zeros((boxes_3d_num, boxes_2d_num, 5), dtype = box_2d_detector.dtype, device = box_2d_detector.device)-1
+            # overlap = torch.zeros((boxes_3d_num, boxes_2d_num, 3), dtype = box_2d_detector.dtype, device = box_2d_detector.device)-1
             
+            #* 用[iou, 3D目标检测分数, 2D目标检测分数, 到LiDAR的距离来填充]
             ious = compute_clocs_iou_sparse(box_2d_preds.contiguous(),
                                         box_2d_detector.contiguous(),
                                         scores_3d,
                                         scores_2d.contiguous(),
                                         dist_to_lidar_single,
                                         overlap)
-            # mask = torch.sum(ious[:, :, 4], dim=-1) !=0
-            # ious[mask, : ,4] = 1
-            # ious = ious.permute(2, 0, 1).view(1, 5, boxes_3d_num, boxes_2d_num)
             ious = ious.permute(2, 0, 1).view(1, 4, boxes_3d_num, boxes_2d_num)
             res = self.fuse(ious)
                 
+            #* 相当于maxpooling
             output = torch.amax(res, dim = -1)
             output = output.squeeze().view(1,-1,1)
             cls_pred_list.append(output)
@@ -113,7 +237,6 @@ class ClocsSECONDHead(AnchorHeadTemplate):
         gt_boxes_classes = np.expand_dims(gt_boxes[:, :, -1], axis=-1)
         gt_boxes_in_camera = np.expand_dims(boxes3d_lidar_to_kitti_camera(gt_boxes[0], data_dict['calib'][0]), axis=0)
         gt_boxes_in_camera = np.concatenate([gt_boxes_in_camera, gt_boxes_classes], axis=-1)
-        preds_in_lidar = copy.deepcopy(preds)
         #* 在相机坐标系下的预测框
         preds_in_camera = np.expand_dims(boxes3d_lidar_to_kitti_camera(preds[0].detach().cpu().numpy(), data_dict['calib'][0]), axis=0)
 
@@ -126,7 +249,7 @@ class ClocsSECONDHead(AnchorHeadTemplate):
             
         if not self.training or self.predict_boxes_when_training:
             data_dict['batch_cls_preds'] = cls_preds
-            data_dict['batch_box_preds'] = preds_in_lidar
+            data_dict['batch_box_preds'] = preds
             data_dict['cls_preds_normalized'] = False
 
         return data_dict
@@ -138,37 +261,43 @@ class ClocsSECONDHead(AnchorHeadTemplate):
         return fusion_loss, tb_dict
     
     def get_cls_layer_loss(self):
+        #* 预测的分数, sigmoid前
         cls_preds = self.forward_ret_dict['cls_preds']
+        #* anchor的标签
         box_cls_labels = self.forward_ret_dict['box_cls_labels']
-        batch_size = int(cls_preds.shape[0])
+        #* 只关心标签大于等于0的, 大于0是正样本, =0是负样本
         cared = box_cls_labels >= 0  # [N, num_anchors]
         positives = box_cls_labels > 0
         negatives = box_cls_labels == 0
-        negative_cls_weights = negatives * 1.0
-        cls_weights = (negative_cls_weights + 1.0 * positives).float()
-        reg_weights = positives.float()
-        if self.num_class == 1:
-            # class agnostic
-            box_cls_labels[positives] = 1
-
-        pos_normalizer = positives.sum(1, keepdim=True).float()
-        reg_weights /= torch.clamp(pos_normalizer, min=1.0)
-        cls_weights /= torch.clamp(pos_normalizer, min=1.0)
+        
+        #* 构建one-hot编码
         cls_targets = box_cls_labels * cared.type_as(box_cls_labels)
-        cls_targets = cls_targets.unsqueeze(dim=-1)
-
-        cls_targets = cls_targets.squeeze(dim=-1)
         one_hot_targets = torch.zeros(
             *list(cls_targets.shape), self.num_class + 1, dtype=cls_preds.dtype, device=cls_targets.device
         )
-        
         one_hot_targets.scatter_(-1, cls_targets.unsqueeze(dim=-1).long(), 1.0)
-        cls_preds = cls_preds.view(batch_size, -1, self.num_class)
         one_hot_targets = one_hot_targets[..., 1:]
-        cls_loss_src = self.cls_loss_func(cls_preds, one_hot_targets, weights=cls_weights)  # [N, M]
-        cls_loss = cls_loss_src.sum() / batch_size
+        #* 使用focal loss
+        focal_loss = SigmoidFocalClassificationLoss()
 
-        cls_loss = cls_loss * self.model_cfg.LOSS_CONFIG.LOSS_WEIGHTS['cls_weight']
+        #* 负样本的权重是1
+        negative_cls_weights = negatives.type(torch.float32) * 1.0
+        #* 正样本的权重也是1
+        cls_weights = negative_cls_weights + 1.0 * positives.type(torch.float32)
+        #* 正样本的个数
+        pos_normalizer = positives.sum(1, keepdim=True).type(torch.float32)
+        
+        #* 权重需要除正样本的个数
+        cls_weights /= torch.clamp(pos_normalizer, min=1.0)
+        
+        #* 用focal loss计算损失
+        cls_losses = focal_loss._compute_loss(cls_preds, one_hot_targets, cls_weights.cuda())  # [N, M]
+        
+        batch_size = int(cls_preds.shape[0])
+        #* 最后的损失需要除以batch size
+        cls_losses = cls_losses.sum()/batch_size
+
+        cls_loss = cls_losses * self.model_cfg.LOSS_CONFIG.LOSS_WEIGHTS['cls_weight']
         tb_dict = {
             'rpn_loss_cls': cls_loss.item()
         }
