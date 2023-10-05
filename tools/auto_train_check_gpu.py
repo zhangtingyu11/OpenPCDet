f303921eb16299cf322ac0d0ac83a77217f2fe99
zhangtingyu11 <18013933973@163.com>
2023-10-06 02:11:19 +0800

    训练程序

--- tools/auto_train_check_gpu.py
+++ tools/auto_train_check_gpu.py
@@ -9,17 +9,16 @@ def get_gpu_memory():
     return gpu_memory
 
 def auto_train():
-    #! 训练最初版本的clocs
-    os.system('python train.py --cfg_file cfgs/kitti_models/second_car_clocs.yaml --fix_random_seed 666 --extra_tag origin_epoch10_seed666' +\
-            ' --pretrained_model ../output/kitti_models/second_car/origin/ckpt/checkpoint_epoch_80.pth')
+    # #! 训练最初版本的clocs
+    # os.system('python train.py --cfg_file cfgs/kitti_models/second_car_clocs.yaml --fix_random_seed 666 --extra_tag origin_epoch10_seed666' +\
+    #         ' --pretrained_model ../output/kitti_models/second_car/origin/ckpt/checkpoint_epoch_80.pth')
     
     #! 添加Contra和添加LA的版本
-    #* USE_GT, VAL_ACTIVATED, PROPOTION, LOWER_SCORE, HIGHER_SCORE, USE_CONTRA, USE_LA
-    no_gt_configs = [[False, False, 0, 0, 0, True, False],
-                     [False, False, 0, 0, 0, False, True]]
-    no_gt_lines = [66, 67, 68, 69, 70, 122, 123]
-    tags = ["use_contra", "use_la"]
-    yaml_file = "/home/zty/Project/DeepLearning/OpenPCDet/tools/cfgs/kitti_models/second_car_clocs_contra.yaml"
+    #* USE_GT, VAL_ACTIVATED, PROPOTION, LOWER_SCORE, HIGHER_SCORE, USE_CONTRA, USE_LA, INPUT_FEATURES
+    no_gt_configs = [[False, False, 0, 0, 0, False, True, 4]]
+    no_gt_lines = [66, 67, 68, 69, 70, 122, 123, 113]
+    tags = ["use_la"]
+    yaml_file = "/home/public/zty/Project/DeepLearningProject/OpenPCDet/tools/cfgs/kitti_models/second_car_clocs_contra.yaml"
     for i, config in enumerate(no_gt_configs):
         tag = tags[i]
         with open(yaml_file, 'r') as f:
@@ -37,20 +36,20 @@ def auto_train():
             
         
     #! 使用gt的版本
-    configs = [[False, False, 0, 0, 0, False, False],
-                [False, False, 0, 0, 0, True, True],
-                [True, False, 0.5, 0.6, 1.0, True, True],
-                [True, False, 0.5, 0.8, 1.0, True, True],
-                [True, False, 1.0, 0.6, 1.0, True, True],
-                [True, False, 1.0, 0.8, 1.0, True, True]]
-    line_idxs = [67, 68, 69, 70, 71, 123, 124]
+    configs = [[False, False, 0, 0, 0, False, False, 4],
+                [False, False, 0, 0, 0, True, True, 5],
+                [True, False, 0.5, 0.6, 1.0, True, True, 5],
+                [True, False, 0.5, 0.8, 1.0, True, True, 5],
+                [True, False, 1.0, 0.6, 1.0, True, True, 5],
+                [True, False, 1.0, 0.8, 1.0, True, True, 5]]
+    line_idxs = [67, 68, 69, 70, 71, 123, 124, 114]
     tags = ["use_gt_sampling",
             "use_all",
             "use_all_0.5_0.6_1.0",
             "use_all_0.5_0.8_1.0"
             "use_all_1.0_0.6_1.0",
             "use_all_1.0_0.8_1.0"]
-    yaml_file = "/home/zty/Project/DeepLearning/OpenPCDet/tools/cfgs/kitti_models/second_car_clocs_contra_fusion_aug.yaml"
+    yaml_file = "/home/public/zty/Project/DeepLearningProject/OpenPCDet/tools/cfgs/kitti_models/second_car_clocs_contra_fusion_aug.yaml"
     for i, config in enumerate(configs):
         tag = tags[i]
         with open(yaml_file, 'r') as f:
@@ -67,9 +66,9 @@ def auto_train():
             ' --pretrained_model ../output/kitti_models/second_car/origin/ckpt/checkpoint_epoch_80.pth')  
         
 if __name__ == "__main__":
-    gpu_memory = get_gpu_memory()[0]
-    while(gpu_memory >100):
-        time.sleep(600)
-        gpu_memory = get_gpu_memory()[0]
+    # gpu_memory = get_gpu_memory()[0]
+    # while(gpu_memory >100):
+    #     time.sleep(600)
+    #     gpu_memory = get_gpu_memory()[0]
     auto_train()
     
